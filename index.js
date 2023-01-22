85309c53f0cc7de1f8d065541e2584998877440d
Braden <braden.clokie@gmail.com>
2023-01-21 22:03:25 -0500

    LightCoin activity, stretch goals are unfinished

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
-let balance = 500.00;
-
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 100;
   }
-
-  commit() {
-    balance -= this.amount;
+  commit(transaction) {
+    this.balance += transaction.value;
   }
-
 }
 
+class Withdrawal {
+  // Pass in the account that the withdrawal this for
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  get value() {
+    return -this.amount;
+  }
+}
 
-
+class Deposit {
+  // Pass in the account that the deposit this for
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+  get value() {
+    return this.amount;
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
+const myAccount = new Account("snow-patrol");
+
+const t1 = new Withdrawal(50.25, myAccount);
+myAccount.commit(t1);
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
+const t2 = new Withdrawal(9.99, myAccount);
+myAccount.commit(t2);
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+const t3 = new Deposit(120.00, myAccount);
+myAccount.commit(t3);
+console.log('Transaction 3:', t3);
+
+console.log('Balance:', myAccount.balance);
